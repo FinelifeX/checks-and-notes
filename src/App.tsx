56eb25850f8b3923ba813f59e5d1cd6ab212c664
Bulat Murtazin <bulat.murtazin@akvelon.com>
2021-01-17 22:34:03 +0300

    + add ui-kitten and eva-design

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { ApplicationProvider } from '@ui-kitten/components';
+import eva from '@eva-design/eva';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <View>
-        <Text>Hello from React Native!</Text>
-      </View>
-    </NavigationContainer>
+    <ApplicationProvider {...eva} theme={eva.dark}>
+      <NavigationContainer>
+        <View>
+          <Text>Hello from React Native!</Text>
+        </View>
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 };
 
